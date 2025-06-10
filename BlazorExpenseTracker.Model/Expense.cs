ffdc4c70e3db3d13db4ba159ea3088bba973bb03
Mauro Lafuente <155067361+MauroLaf@users.noreply.github.com>
2025-06-10 22:19:03 +0200

    validations

--- BlazorExpenseTracker.Model/Expense.cs
+++ BlazorExpenseTracker.Model/Expense.cs
@@ -1,9 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using BlazorExpenseTracker.Model.Validations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BlazorExpenseTracker.Model
 {
-    public class Expense
+    public class Expense : IValidatableObject
     {
         [Key]
         public int Id { get; set; }
@@ -19,8 +20,27 @@ namespace BlazorExpenseTracker.Model
         public Category Category { get; set; }
 
         [Required]
+        [ExpenseTransactionDateValidator(DaysInTheFuture = 30)]
         public DateTime TransactionDate { get; set; }
         [Required]
         public ExpenseType ExpenseType { get; set; }
+
+        //al agregar la interfaz de validation puedo crera validaciones específicas
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var errors = new List<ValidationResult>();
+            if (ExpenseType == ExpenseType.Income && Amount < 0)
+            {
+                errors.Add(new ValidationResult("Income amount cannot be negative.",
+                    new[] { nameof(Amount) }));
+            }
+            else if (ExpenseType == ExpenseType.Expense && Amount > 0)
+            {
+                errors.Add(new ValidationResult("Expense amount must be negative.",
+                    new[] { nameof(Amount) }));
+            }
+            return errors;
+        }
     }
 }
---
+++ BlazorExpenseTracker.Model/Validations/ExpenseTransactionDateValidator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorExpenseTracker.Model.Validations
+{
+    public class ExpenseTransactionDateValidator: ValidationAttribute
+    {
+        public int DaysInTheFuture { get; set; }
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            DateTime transactionDate;
+            if(DateTime.TryParse(value.ToString(), out transactionDate)) //el resultado del parseo lo pongo en transactionDate
+            {
+                if (transactionDate == DateTime.MinValue)
+                {
+                    return new ValidationResult("Transaction date cannot be empty.",
+                        new[] { validationContext.MemberName });
+                }
+                else if (transactionDate > DateTime.Now.AddDays(DaysInTheFuture))
+                {
+                    return new ValidationResult($"Transaction date cannot be more than {DaysInTheFuture} days in the future.",
+                        new[] { validationContext.MemberName });
+                }
+                return null;
+            }
+
+            return new ValidationResult("Invalid date.",
+                new[] { validationContext.MemberName });
+        }
+    }
+}
